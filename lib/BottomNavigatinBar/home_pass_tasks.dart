50b19074afd6f1bd0d05398a287697c1188aeacc
平尾悠喜 <c1400809@st.kanazawa-it.ac.jp>
2024-12-17 21:19:56 +0900

    ファイル消去

---
+++ lib/BottomNavigatinBar/home_pass_tasks.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+// 削除済みタスクを表示するクラス
+class CompletedTasksPage extends StatelessWidget {
+  final List<String> deletedTasks;
+
+  CompletedTasksPage({required this.deletedTasks}); // コンストラクタでデータを受け取る
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("削除済みタスク")),
+      body: ListView.builder(
+        itemCount: deletedTasks.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(deletedTasks[index]), // 削除済みタスクを表示
+          );
+        },
+      ),
+    );
+  }
+}
