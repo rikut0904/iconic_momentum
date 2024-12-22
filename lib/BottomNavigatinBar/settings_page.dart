1c72ead24db237e2c935386a6b9b67a03f2b19f3
rikut0904 <rkt9tuba4robot@gmail.com>
2024-12-22 11:21:06 +0900

    ログアウト機能の実装

--- lib/BottomNavigatinBar/settings_page.dart
+++ lib/BottomNavigatinBar/settings_page.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:iconic_momentum/main.dart';
+import 'package:iconic_momentum/BottomNavigatinBar/login_page.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-typedef TodoItem = ({String title, bool done});
-
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
 
+  @override
+  State<SettingsPage> createState() => _SettingPage();
+}
+
+class _SettingPage extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,15 +22,39 @@ class SettingsPage extends StatelessWidget {
           ),
           backgroundColor: Colors.purple[300], // AppBarの色
         ),
-        body: const Padding(
-          padding: EdgeInsets.all(16.0),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              Text('設定画面-以下記述するもの-', style: TextStyle(fontSize: 24)),
-              Text('githubリンク公開', style: TextStyle(fontSize: 18)),
-              Text('プライバシーポリシー', style: TextStyle(fontSize: 18)),
-              Text('ログアウトボタン', style: TextStyle(fontSize: 18)),
+              const Text('設定画面-以下記述するもの-', style: TextStyle(fontSize: 24)),
+              const Text('githubリンク公開', style: TextStyle(fontSize: 18)),
+              const Text('プライバシーポリシー', style: TextStyle(fontSize: 18)),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    icon: const Icon(Icons.logout),
+                    color: Colors.purple,
+                    iconSize: 30,
+                    tooltip: 'ログアウト',
+                    onPressed: () async {
+                      await FirebaseAuth.instance.signOut();
+                      if (context.mounted) {
+                        Navigator.of(context).pushReplacement(
+                          MaterialPageRoute(builder: (context) {
+                            return const LoginPage();
+                          }),
+                        );
+                      }
+                    },
+                  ),
+                  const Text(
+                    'ログアウト',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ],
+              ),
             ],
           ),
         ));
